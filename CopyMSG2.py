2d5b003155a7a142318fa2137c5a963f4e397694
Deliaaan <delian.ngtmr@gmail.com>
2023-03-15 23:35:43 -0600

    2dn modification discord bot/ requiere admin permision

--- CopyMSG2.py
+++ CopyMSG2.py
@@ -2,19 +2,27 @@ import discord
 import asyncio
 
 # Coloca el token de tu bot de Discord aquí
-token = 'AQUI_PEGA_EL_TOKEN_DE_TU_BOT_DE_DISCORD'
+token = 'MTA4NTc3NzQ2MjYwNjE4MDQzMg.Gsuj8V.hv-KD5pxG2FQpnbmTw7fk_wRmKa2kTbnbYJsLw'
+server_id = '744338116332093523'
 
 # Coloca el ID del canal de origen aquí
-canal_origen_id = 1234567890
+canal_origen_id = 831991368867512431
 
 # Coloca el ID del canal de destino aquí
-canal_destino_id = 0987654321
+canal_destino_id = 1085790776283385906
 
-client = discord.Client()
+
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print('El bot está listo y conectado al servidor de Discord!')
+
+    # Obtener los canales del servidor
+    server = client.get_guild(server_id)
+
 
 @client.event
 async def on_message(message):
